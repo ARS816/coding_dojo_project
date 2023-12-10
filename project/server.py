d8867e30e408f5706117babeb97f8d7622e89384
Aimee Santone <132028651+ARS816@users.noreply.github.com>
2023-12-09 21:24:34 -1000

    Delete project directory

--- project/server.py
+++
@@ -1,9 +0,0 @@
-from flask_app import app
-from flask_app.controllers import challenges
-from flask_app.controllers import users
-from flask_app.controllers import home
-from flask_app.controllers import posts
-
-
-if __name__ == "__main__":
-    app.run(debug = True, host = "localhost", port = 8000)
